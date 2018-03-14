a0cf4553acce703f78663a886c2c02b5ea3aa4e3
Development@IntoVirtual <37337323+IntoVirtualDevelopment@users.noreply.github.com>
2018-03-14 13:48:19 +0100

    Multiplayer steps

--- HololensApplicatie/Assets/CameraReceiver.cs
+++ HololensApplicatie/Assets/CameraReceiver.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class CameraReceiver :   {
+public class CameraReceiver : NetworkBehaviour {
+	
 	public static CameraReceiver instance;
 
+	public Texture2D texture;
+
 	private void Start() {
-		instance = this;
+        instance = this;
+        texture = new Texture2D(50, 50);
 	}
 
 	[Client]
 	public void Receive(byte[] pixels) {
-
+		texture.LoadRawTextureData(pixels);
+        texture.Apply();
 	}
 }
---
+++ HololensApplicatie/Assets/DebugHololensInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.UI;
+
+public class DebugHololensInput : MonoBehaviour {
+
+	GestureRecognizer gr;
+
+	void Start() {
+		gr = new GestureRecognizer();
+
+		gr.Tapped += OnTap;
+
+		gr.StartCapturingGestures();
+	}
+
+	void OnTap(TappedEventArgs args) {
+        
+	}
+}
